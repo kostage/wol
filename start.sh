7de304140f54ba08293fb8df1ec5814f842b2d25
Your Name <you@example.com>
2025-04-02 01:37:39 +0300

    etherwake and ling via sockets

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# Prepare config
-export APP_PATH="$PATH"
-envsubst < /etc/lighttpd/lighttpd.conf > /tmp/lighttpd_patched.conf
-
 # Set permissions for logs
 chown appuser:appuser /var/log/lighttpd /var/log/lighttpd/*.log 2>/dev/null || true
 
 # Start Lighttpd
-exec lighttpd -D -f /tmp/lighttpd_patched.conf
+exec lighttpd -D -f /etc/lighttpd/lighttpd.conf
--- test/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run unit tests
-echo "Running unit tests..."
-bats /test/unit
-
-# Run integration tests
-echo "Running integration tests..."
-bats /test/integration
